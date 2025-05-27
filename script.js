ff0de296b70e1f3bef40fb7be9bae9b8d3ad5e3e
Anna Lukina <Liska_148@mail.ru>
2025-05-27 17:50:29 +0300

    Добавила скрипт

---
+++ script.js
@@ -0,0 +1,143 @@
+"use strict";
+// console.log('Hello world!!')
+
+// let catName = 'Барсик';
+// catName = 'Барсик';
+
+// const catName = 'Барсик';
+// console.log(catName)
+
+// catName='Барсона'
+// console.log(catName)
+
+// устаревшее
+// var catName = 'Барс'
+// console.log(catName)
+
+// let str = 'строка';
+// let nmb = 'число';
+// let logic = 'логический';
+
+// console.log(typeof(str))
+
+// const catGrey = {name:'Барсик', age:'2', toys:[1,1323, 22], master:{name:'dsfdfg', age:25}};
+
+// let age = prompt('Сколько вам лет?');
+
+// console.log(typeof age);
+
+// if(Number(age) === 18){
+//   alert('Вы можете читать статьи на этом сайте')
+// }
+// // else if(age == 18){
+// //   alert('Вы совершеннолетний! Поздравляем!')
+// // }
+// else{
+//   alert('Вам нельзя посещать этот сайт')
+
+// }
+
+// let age = prompt('Сколько вам лет?');
+// const isAdult = Number(age) > 18;
+// const message = isAdult ? 'Вы можете читать статьи на этом сайте': 'Вам нельзя посещать этот сайт';
+// alert(message)
+
+// let catKind = 'Manul';
+
+// switch (catKind) {
+//   case 'Gepard':{
+//     console.log('Мощный хищник')
+//     break;
+//   }
+//    case 'Manul':{
+//     console.log('Мохнатый и милый')
+//     break;
+//   } case 'Lev':{
+//     console.log('Ленивый царь зверей')
+//     break;
+//   }
+//   default:
+//     console.log('Неизвестный коть')
+//     break;
+// }
+
+// const numbers = [1,2,3,4,5];
+// let i = 0;
+
+// while(i < numbers.length){
+//   const currentElement = numbers[i];
+//   console.log(currentElement*2)
+//   i++
+// }
+
+// do {
+//   const currentElement = numbers[i];
+//   console.log(currentElement*2)
+//   i++
+// } while(i < numbers.length)
+
+// for(let i = numbers.length-1; i>=0; i--){
+//   if(numbers[i] === 3){
+//     // continue;
+//     break;
+//   }
+//   console.log(numbers[i]*2);
+// }
+
+// sayHiDeclaration('Anna')
+
+// function declaration
+// function sayHiDeclaration(name){
+//   // console.log("Привет "+name);
+
+//   console.log(`Привет ${name}`)
+// }
+
+// // sayHiDeclaration('Anna')
+
+// sayHiExpression('Петя')
+// // // Function Expression
+// const sayHiExpression = function (name) {
+//   console.log(`Привет ${name}`)
+// }
+
+const links = [
+  {
+    name:  'Главная' ,
+    link:'./index.html'
+  },
+  {
+    name:  'Блог' ,
+    link:'./index.html'
+  },
+   {
+    name:  'Проекты' ,
+    link:'./index.html'
+  },
+   {
+    name:  'Доклады' ,
+    link:'./index.html'
+  },
+   {
+    name:  'Резюмешечка для работодателей' ,
+    link:'./index.html'
+  },
+]
+
+//  <ul class="navigation">
+//         <li><a href="./index.html">Главная</a></li>
+//         <li><a href="./blog.html">Блог</a></li>
+//         <li><a href="./projects.html">Проекты</a></li>
+//         <li><a href="./presentations.html">Доклады</a></li>
+//         <li><a href="./resume.html">Резюмешечка для работодателей</a></li>
+//       </ul>
+
+let navbar = document.querySelector('.navigation');
+// console.log(navbar)
+navbar.innerHTML=`
+  <li><a href="./index.html">Обо мне</a></li>
+        <li><a href="./blog.html">Блог</a></li>
+        <li><a href="./projects.html">Проекты</a></li>
+        <li><a href="./presentations.html">Доклады</a></li>
+        <li><a href="./resume.html">Резюмешечка для работодателей</a></li>
+`
